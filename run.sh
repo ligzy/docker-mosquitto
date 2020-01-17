1c04253232ee06b87b6214192276827e934a1873
Jeremy Li <lizhiyong1@soundai.com>
2020-01-17 17:12:29 +0800

    run updated.

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -e
 
 chown mosquitto:mosquitto -R /var/lib/mosquitto
+chown mosquitto:mosquitto -R /var/log/mosquitto
+chown mosquitto:mosquitto -R /opt/mosquitto
 
 if [ "$1" = 'mosquitto' ]; then
 	exec /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
